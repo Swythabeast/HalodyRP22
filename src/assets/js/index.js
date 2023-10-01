8e6fb72e66432d231849df1db1a0699a83a0f8e4
JusDePoire <antonin@dutreix.net>
2023-10-01 11:23:17 +0200

    Changement des Splashtexts au lancements

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -22,9 +22,9 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n' ai pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "Charchement du charabia numérique", "author": "JusDePoire" },
+            { "message": "Cuisson de l'Euphillium", "author": "JusDePoire" },
+            { "message": "Votre aventure est sur le point de débuter...", "author": "JusDePoire" }
         ]
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
